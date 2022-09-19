abe2d89cfe63d5b92f5cd9f19f418a4ffd490b17
chobkyu <qudqud97@naver.com>
2022-09-19 16:09:10 +0900

    헤더 세팅

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/"/>   
+          
+          <Route path="/doing"/>
+         
+          <Route  path="/complete"/>
+           
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ my-app/src/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+function Header(){
+    return (
+        <div className="container">
+        <header>
+          <nav>
+            <div className="icon">TODO List</div>
+            <div className="gnb">
+              <Link to="/">해야될 일</Link>
+              <Link to="/doing">하고 있는 일</Link>
+              <Link to="/complete">완료한 일</Link>
+            </div>
+          </nav>
+        </header>
+      </div>
+    )
+}
+
+export default Header;
